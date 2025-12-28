46e73cffac9990d3f2e0fe5b8ee205dffdbbac5c
Rashik Prajapati <rshkprjpt@gmail.com>
2025-12-28 07:49:38 +0545

    chore: remove dist outputs from packages/common

--- packages/common/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./env";
-export * from "./logger";
-export { z } from "zod";
-export type { Logger } from "pino";
-//# sourceMappingURL=index.d.ts.map
--- packages/common/dist/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./env";
-export * from "./logger";
-export { z } from "zod";
-//# sourceMappingURL=index.js.map
